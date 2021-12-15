165d1c734e0e06398e8a21426d8a9cf8910ddfc1
mrprizmo <anton.dorosev@yandex.ru>
2021-12-15 20:32:34 +0300

    version 1.0.1

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import sys
+from logging.config import dictConfig
+import logging
 import requests
 from PyQt5 import uic
 from typing import Any
-from PyQt5.QtGui import QPixmap, QPainter, QColor, QBrush, QPolygonF
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QBrush, QPolygonF, QPen, QPainterPath
 from PyQt5.QtCore import Qt, QEvent, QRect, QPointF
 from PyQt5.QtWidgets import QApplication, QMainWindow
+
+dictLogConfig = {
+        'version': 1,
+        "handlers": {
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "standard",
+                "filename": "logging.log",
+                "mode": 'w+'
+            }
+        },
+        "loggers": {
+            "Displayer": {
+                "handlers": ["fileHandler"],
+                "level": "INFO",
+            }
+        },
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            }
+        }
+    }
+
+dictConfig(dictLogConfig)
+log = logging.getLogger("Displayer")
 
 
 class FigmaDisplayer(QMainWindow):
@@ -20,7 +48,8 @@ class FigmaDisplayer(QMainWindow):
         self.pixmap = QPixmap(self.width, self.height)
         self.canvas.setPixmap(self.pixmap)
         self.need_to_update = 1
-        self.offset_x = self.offset_y = self.scale = 0
+        self.offset_x = self.offset_y = 0
+        self.scale = 1
         self.response = []
         self.update()
 
@@ -32,34 +61,60 @@ class FigmaDisplayer(QMainWindow):
             if not self.response:
                 qp.drawText(QRect(0, 0, self.width, self.height), Qt.AlignCenter, "No objects")
             for obj in self.response:
-                x, y = (obj['absoluteBoundingBox']['x'] - self.offset_x) * self.scale, (obj['absoluteBoundingBox']['y'] - self.offset_y) * self.scale
-                width, height = obj['absoluteBoundingBox']['width'] * self.scale, obj['absoluteBoundingBox']['height'] * self.scale
-                box = QRect(x, y, width, height)
-                color = QColor(obj['fills'][0]['color']['r'] * 255, obj['fills'][0]['color']['g'] * 255,
-                               obj['fills'][0]['color']['b'] * 255)
-                brush = QBrush(color, Qt.SolidPattern)
-                qp.setBrush(brush)
+                try:
+                    x, y = (obj['absoluteBoundingBox']['x'] - self.offset_x) * self.scale, (
+                            obj['absoluteBoundingBox']['y'] - self.offset_y) * self.scale
+                    width, height = obj['absoluteBoundingBox']['width'] * self.scale, obj['absoluteBoundingBox'][
+                        'height'] * self.scale
+                    box = QRect(x, y, width, height)
+                    color = QColor(obj['fills'][0]['color']['r'] * 255, obj['fills'][0]['color']['g'] * 255,
+                                   obj['fills'][0]['color']['b'] * 255, obj['fills'][0]['color']['a'] * 255)
 
-                if obj['shapeType'] == "ELLIPSE":
-                    qp.drawEllipse(box)
-                elif obj['shapeType'] == "SQUARE" or obj['shapeType'] == "ROUNDED_RECTANGLE":
-                    qp.drawRoundedRect(box, 15, 15)
-                elif obj['shapeType'] == "TRIANGLE_DOWN":
-                    points = (QPointF(x, y), QPointF(x + width, y), QPointF(x + width // 2, y + height))
-                    qp.drawPolygon(QPolygonF(points))
-                elif obj['shapeType'] == "PARALLELOGRAM_RIGHT":
-                    points = (
-                        QPointF(x + width // 5, y), QPointF(x + width, y), QPointF(x + 4 * width // 5, y + height),
-                        QPointF(x, y + height)
-                    )
-                    qp.drawPolygon(QPolygonF(points))
-                elif obj['shapeType'] == "PARALLELOGRAM_LEFT":
-                    points = (
-                        QPointF(x, y), QPointF(x + 4 * width // 5, y), QPointF(x + width, y + height),
-                        QPointF(x + width // 5, y + height)
-                    )
-                    qp.drawPolygon(QPolygonF(points))
-                qp.drawText(box, Qt.AlignCenter, obj['characters'])
+                    if obj['type'] == "RECTANGLE":
+                        qp.setBrush(QBrush(color, Qt.SolidPattern))
+                        qp.setPen(QPen(color, 1, Qt.SolidLine))
+                        if obj.get('shapeType'):
+                            if obj['shapeType'] == "ROUNDED_RECTANGLE":
+                                qp.drawRoundedRect(box, 15, 15)
+                        else:
+                            qp.drawRect(box)
+                        """
+                        roundedpath = QPainterPath()
+                        roundedpath.setFillRule(Qt.WindingFill)
+                        roundedpath.addEllipse(box)
+                        roundedpath.addRoundedRect(x, y + height // 2, width // 2, height // 2, ???)
+                        """
+
+                    elif obj['type'] == "SHAPE_WITH_TEXT":
+                        qp.setBrush(QBrush(color, Qt.SolidPattern))
+                        qp.setPen(QPen(color, 1, Qt.SolidLine))
+                        if obj['shapeType'] == "ELLIPSE":
+                            qp.drawEllipse(box)
+                        elif obj['shapeType'] == "SQUARE" or obj['shapeType'] == "ROUNDED_RECTANGLE":
+                            qp.drawRoundedRect(box, 15, 15)
+                        elif obj['shapeType'] == "TRIANGLE_DOWN":
+                            points = (QPointF(x, y), QPointF(x + width, y), QPointF(x + width // 2, y + height))
+                            qp.drawPolygon(QPolygonF(points))
+                        elif obj['shapeType'] == "PARALLELOGRAM_RIGHT":
+                            points = (
+                                QPointF(x + width // 5, y), QPointF(x + width, y),
+                                QPointF(x + 4 * width // 5, y + height),
+                                QPointF(x, y + height)
+                            )
+                            qp.drawPolygon(QPolygonF(points))
+                        elif obj['shapeType'] == "PARALLELOGRAM_LEFT":
+                            points = (
+                                QPointF(x, y), QPointF(x + 4 * width // 5, y), QPointF(x + width, y + height),
+                                QPointF(x + width // 5, y + height)
+                            )
+                            qp.drawPolygon(QPolygonF(points))
+                        qp.setPen(QPen(QColor(0, 0, 0), 12, Qt.SolidLine))
+                        qp.drawText(box, Qt.AlignCenter, obj['characters'])
+
+                    elif obj['type'] == "TEXT":
+                        pass
+                except BaseException as exeption:
+                    log.exception(f"ERROR:\n{exeption.args}")
 
             qp.end()
             self.canvas.setPixmap(self.pixmap)
@@ -71,25 +126,28 @@ class FigmaDisplayer(QMainWindow):
         header['X-FIGMA-TOKEN'] = self.line_access_token.text().strip()
         header['Content-Type'] = 'application/json'
         response = requests.get(request.format(self.line_project_name.text().strip()), headers=header)
+        log.info(f"RESPONSE:\n{response.json()}")
         if response:
             self.need_to_update = 1
             try:
                 response = response.json()['document']['children'][0]['children']
                 self.response = []
-                x = []
-                y = []
+                x = [0]
+                y = [0]
                 for obj in response:
-                    if obj['type'] == "SHAPE_WITH_TEXT":
-                        self.response.append(obj)
-                        x.append(obj['absoluteBoundingBox']['x'])
-                        y.append(obj['absoluteBoundingBox']['y'])
-                        x.append(obj['absoluteBoundingBox']['x'] + obj['absoluteBoundingBox']['width'])
-                        y.append(obj['absoluteBoundingBox']['y'] + obj['absoluteBoundingBox']['height'])
+                    self.response.append(obj)
+                    x.append(obj['absoluteBoundingBox']['x'])
+                    y.append(obj['absoluteBoundingBox']['y'])
+                    x.append(obj['absoluteBoundingBox']['x'] + obj['absoluteBoundingBox']['width'])
+                    y.append(obj['absoluteBoundingBox']['y'] + obj['absoluteBoundingBox']['height'])
                 self.offset_x = min(x)
                 self.offset_y = min(y)
-                self.scale = min(self.width / (max(x) - self.offset_x), self.height / (max(y) - self.offset_y))
-            except BaseException:
-                pass
+                if max(x) - self.offset_x != 0 and max(y) - self.offset_y != 0:
+                    self.scale = min(self.width / (max(x) - self.offset_x), self.height / (max(y) - self.offset_y))
+                else:
+                    self.scale = 1
+            except BaseException as exeption:
+                log.exception(f"ERROR:\n{exeption.args}")
 
 
 def except_hook(cls: Any, exception: Any, traceback: Any) -> None:
